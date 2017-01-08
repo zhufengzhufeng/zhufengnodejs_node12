23ad3943d3275c0f9cfaf43dbaca308c99c6c15f
zhufengzhufeng <894918097@qq.com>
2017-01-08 15:35:52 +0800

    server

--- 11.http/3.route.js
+++ 11.http/3.route.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');//解析路径的
+var mime = require('mime');
 var server = http.createServer(function (req,res) {
     console.log(req.headers["cache-control"]); //请求头信心小写取出来
     console.log(req.method);//通过浏览器访问默认是Get请求
@@ -10,10 +11,27 @@ var server = http.createServer(function (req,res) {
     if(pathname=='/'){ //默认直接访问为/ 直接响应内容即可
         res.setHeader('Content-Type','text/html;charset=utf8');
         fs.createReadStream('./index.html').pipe(res);
+    }else{
+        //mime专门处理content-type 第三方模块,查看路径的后缀名生成对应的类型
+        //先判断文件是否存在
+        fs.exists('.'+pathname,function (flag) {
+            if(flag){
+                res.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf8');
+                fs.createReadStream('.'+pathname).pipe(res);
+            }else{
+                res.statusCode = 404;
+                res.end('Not Found');
+            }
+        });
+    }
+/*
+    if(pathname == '/index.html'){
+        res.setHeader('Content-Type','text/html;charset=utf8');
+        fs.createReadStream('./index.html').pipe(res);
     }
     if(pathname=='/index.css'){ //默认直接访问为/ 直接响应内容即可
         res.setHeader('Content-Type','text/css;charset=utf8');
         fs.createReadStream('./index.css').pipe(res);
-    }
+    }*/
 });
 server.listen(80);
---
+++ 11.http/server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var mime = require('mime');//第三方
+http.createServer(function (req,res) {
+    var urlObj = url.parse(req.url,true);
+    var pathname = urlObj.pathname;
+    if(pathname == '/'){
+        res.setHeader('Content-Type','text/html;charset=utf8');
+        fs.createReadStream('./index.html').pipe(res);
+    }else{
+        fs.exists('.'+pathname,function (flag) {
+            if(flag){
+                res.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf8');
+                fs.createReadStream('.'+pathname).pipe(res);
+            }else{
+                res.statusCode = 404;
+                res.end('Not Found');
+            }
+        })
+    }
+}).listen(3000);
