a512d58c9aec66af238b1be5d5eb943e75f97352
zhufengzhufeng <894918097@qq.com>
2017-01-08 17:04:10 +0800

    manager

---
+++ 11.http/manager/server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var mime = require('mime');
+var userList = [
+    {username:'张三',password:'xxxx',id:1},
+];
+http.createServer(function (req,res) {
+    var urlObj = url.parse(req.url,true);
+    var pathname = urlObj.pathname;
+    if(pathname == '/'){
+        res.setHeader('Content-Type','text/html;charset=utf8');
+        fs.createReadStream('./index.html').pipe(res);
+    }else if(pathname == '/getUsers'){
+        res.end(JSON.stringify(userList));
+    }else if(pathname == '/addUser'){
+
+    }else if(pathname == '/deleteUser'){
+
+    }else{
+        fs.exists('.'+pathname,function (flag) {
+            if(flag){
+                res.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf8');
+                fs.createReadStream('.'+pathname).pipe(res);
+            }else{
+                res.statusCode = 404;
+                res.end('Not Found');
+            }
+        });
+    }
+}).listen(3000);
